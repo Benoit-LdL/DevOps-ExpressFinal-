21ef143937c6b332fb0c1e90a4eaa526dffffc5c
Benoît <benoitlagasse@hotmail.com>
2020-05-21 16:28:01 +0200

    bugfix

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 var app = require('../app_oauth.js');
-describe('GET /', function() {
+describe('GET /test', function() {
  it('respond with hello world', function(done) {
  //navigate to root and check the the response is "hello world"
  request(app).get('/').expect('hello world', done);
