ed94d23c41d973147dea5d2dfa26ef4f329bae91
Benoît <benoitlagasse@hotmail.com>
2020-05-21 16:37:04 +0200

    added scripts folder with first test script

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../app_oauth.js');
 describe('GET /test', function() {
- it('respond with hello world', function(done) {
+ it('respond with redirecting to login', function(done) {
  //navigate to root and check the the response is "hello world"
  request(app).get('/').expect('Found. Redirecting to login.html', done);
  });
