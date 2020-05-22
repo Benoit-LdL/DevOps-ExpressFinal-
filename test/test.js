a2ef77cda2e0245f7a41085b69efe21f2590063f
Benoît <benoitlagasse@hotmail.com>
2020-05-22 11:57:36 +0200

    bug fix

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app_oauth.js');
 describe('GET /test', function() {
  it('respond with redirecting to login', function(done) {
- //navigate to root and check the the response is "hello world"
+ //navigate to root and check the the response is "Found. Redirecting to login.html"
  request(app).get('/').expect('Found. Redirecting to login.html', done);
  });
 });
